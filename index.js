ffbca63bd32187ecfba08801518bdc862f7bbe5e
Thomas Docu <thomas.docu0@gmail.com>
2017-06-02 14:45:32 -0400

    stuff

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+'use strict'
+
 require('dotenv').config();
 const express        = require('express');
+const router         = express.Router();
 const logger         = require('morgan');
 const path           = require('path');
 const bodyParser     = require('body-parser');
@@ -9,3 +12,9 @@ const methodOverride = require('method-override');
 const PORT = process.argv[2] || process.argv.PORT || 3000;
 
 const app = express();
+
+app.set("view.engine", "ejs")
+
+app.use(router)
+
+app.listen(3000, () => console.log("running"));
