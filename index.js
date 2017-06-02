c8d1c88af944339ed70bf0057ec2b95e669afe07
Thomas Docu <thomas.docu0@gmail.com>
2017-06-02 16:08:37 -0400

    wip broken routing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const express        = require('express');
-const router         = express.Router();
+const router         = require('./router.js');
 const logger         = require('morgan');
 const path           = require('path');
 const bodyParser     = require('body-parser');
--- router.js
+++ router.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // ROUTER middleware (router.use)
-router.use("/", require('./controllers/home'))
-router.use("/posts", require("./controllers/posts/controller.js"));
+router.use("/", require("./controllers/posts/controller.js"));
 // router.use("comments", require("./controllers/"));
 // Export module to reuse throughout app
 module.exports = router;
